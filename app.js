566f0b6463ac097717c254cd45c4e3e6b7c2dcb9
ryanshz <92750669+ryanshz@users.noreply.github.com>
2024-02-23 20:34:06 -0500

    added general error handler

--- app.js
+++ app.js
@@ -2,25 +2,38 @@ const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 
+//routes
 const itemRoutes = require('./routes/itemRoutes');
 const newRoutes = require('./routes/newRoutes');
 
+//base stuff
 const app = express();
 let port = 3000;
 let hostname = 'localhost';
 app.set('view engine', 'ejs');
 
+//middleware
 app.use(morgan('tiny'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+//routes
 app.get('/', (req, res) => {
     res.render('index', { title: 'Home' });
 });
-
 app.use('/items', itemRoutes);
 app.use('/new', newRoutes);
+
+//error handler
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    if(!err.status){
+        err.status = 500;
+        err.message = 'hey lol something broke on my end! my bad!';
+    }
+    res.status(err.status).render('error', { error: err });
+});
 
 app.listen(port, hostname, () => {
     console.log(`Server is running at http://${hostname}:${port}/`);
